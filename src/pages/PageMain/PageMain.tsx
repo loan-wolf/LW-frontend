a6b14f424f77d7fad15321cd371dc26facb7b050
Pavel Filippov <zanziver@gmail.com>
2021-09-06 09:55:06 +0200

    upd

--- src/pages/PageMain/PageMain.tsx
+++ src/pages/PageMain/PageMain.tsx
@@ -1,5 +1,23 @@
 import { ContentBox } from 'shared/ui/layout/ContentBox'
+import { useWalletInfo } from 'modules/wallet/hooks/useWalletInfo'
+import { ethers } from 'ethers'
+import { createContractHelpers } from 'modules/blockChain/utils/createContractHelpers'
+
+async function fff(addr: any) {
+  console.log(createContractHelpers)
+  const body = await fetch('./TestDai.json')
+  const TokenArtifact = await body.json()
+  // if (addr) {
+  //   let contract = new ethers.Contract(
+  //     '0x57A9E5b84b9b9D096E97E959a73f0aBe8b05D75c',
+  //     TokenArtifact.abi,
+  //     addr,
+  //   )
+  // }
+}
 
 export function PageMain() {
-  return <ContentBox>Main page</ContentBox>
+  const { walletAddress: address } = useWalletInfo()
+  fff(address)
+  return <ContentBox>Main page!!!</ContentBox>
 }
