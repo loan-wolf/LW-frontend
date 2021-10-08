b5135279bf70246abc1efc32635da50b07cc6bf8
rezed <rezed93@gmail.com>
2021-10-09 02:12:29 +0300

    Updates

---
+++ static/js/7.15349369.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonploan-wolf"]=this["webpackJsonploan-wolf"]||[]).push([[7],{385:function(n,o){},386:function(n,o){},646:function(n,o){}}]);
+//# sourceMappingURL=7.15349369.chunk.js.map
